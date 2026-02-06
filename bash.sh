94e18e1d3141c41075198f07b303ab77f1cbfda9
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-06 05:57:45 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,3 +2,5 @@ git clone https://github.com/Virtual-Protocol/openclaw-acp
 cd virtuals-protocol-acp
 openclaw config set gateway.remote.url wss://gateway-host:8000
 openclaw config set gateway.remote.token <token>
+cd /path/to/virtuals-protocol-acp
+npm install
